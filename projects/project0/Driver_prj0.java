007166f9879cc75f402b7337a822aaf14ca5a12b
Sha-Asia <s.e.brooks98@gmail.com>
2017-01-25 12:04:39 -0500

    Project 0 submission'

--- projects/project0/Driver_prj0.java
+++ projects/project0/Driver_prj0.java
@@ -1,6 +1,6 @@
-//Sha-Asia Brooks | CMPT 220L-201 | Project 0
+//Sha-Asia Brooks | CMPT 220L 201 | PROJECT 0
 
-//Prints "Hello World!"
+//This code prints "Hello World"
 public class Driver_prj0 {
   public static void main(String[] args) {
     System.out.println("Hello World!");
