22407b12e6295fa34adfb6b44003114262b2c78a
Sha-Asia <s.e.brooks98@gmail.com>
2017-02-07 23:02:21 -0500

    Proper heading added to Project 0.

--- projects/project0/Driver_prj0.java
+++ projects/project0/Driver_prj0.java
@@ -1,5 +1,14 @@
-//Sha-Asia Brooks | CMPT 220L 201 | PROJECT 0
-
+  /**
+   * file: Driver_prj0.java
+   * author: Sha-Asia Brooks
+   * course: CMPT 220
+   * assignment: Project 0
+   * due date: January 24, 2017
+   * version: 1.0
+   * 
+   * This file contains my solution to Project 0.
+   * 
+   */
 //This code prints "Hello World"
 public class Driver_prj0 {
   public static void main(String[] args) {
